440e20e6e63420529e4ff383ddd94ac52e02f960
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-03 13:40:17 -0300

    First Commit

---
+++ src/main/java/com/github/SarahTumenas/restintermediario/RestIntermediarioApplication.java
@@ -0,0 +1,13 @@
+package com.github.SarahTumenas.restintermediario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestIntermediarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestIntermediarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/SarahTumenas/restintermediario/RestIntermediarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SarahTumenas.restintermediario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestIntermediarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
